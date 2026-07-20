54d82762162994e85d7ac54a6426ebd6426939ba
jaredhansen0128 <jaredhansen0128@gmail.com>
2026-07-20 10:23:53 -0400

    SQL: Add term_bin to model view

--- sql/06_create_model_view.sql
+++ sql/06_create_model_view.sql
@@ -5,7 +5,14 @@ DROP VIEW IF EXISTS sba.vw_loans_model;
 CREATE VIEW sba.vw_loans_model AS
 
 SELECT v.*, 
-       CASE WHEN r.collateral_ind = 'Y' THEN 1 ELSE 0 END AS collateral_ind
+       CASE WHEN r.collateral_ind = 'Y' THEN 1 ELSE 0 END AS collateral_ind,
+       CASE WHEN v.term_in_months BETWEEN 0 AND 60 THEN '0-60mo'
+              WHEN v.term_in_months BETWEEN 61 AND 84 THEN '61-84mo'
+              WHEN v.term_in_months BETWEEN 85 AND 120 THEN '85-120mo'
+              WHEN v.term_in_months BETWEEN 121 AND 240 THEN '121-240mo'
+              WHEN v.term_in_months BETWEEN 241 AND 300 THEN '241-300mo'
+              WHEN v.term_in_months BETWEEN 301 AND 324 THEN '301-324mo'
+              END AS term_bin
 FROM sba.vw_loans_clean AS v
 JOIN sba.loans_raw AS r
 USING(loan_id)
